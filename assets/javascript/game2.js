c5c4a69c086960529b8b527d7381dfc7385f8f47
Lisa Ewart <ewartle@gmail.com>
2017-11-20 13:03:26 -0500

    This commit separates style sheets

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,65 +1,225 @@
+
+
 $(document).ready(function() {
-
+      
+    
   var wins=0;
-  var guessCount=12;
-  var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+  var correctGuess=0;
+  var guessCount=8;
+  var usedWordsList =[];
+  var usedGuess = [];
+  
+  var wordList = [ ["h", "o", "r", "s", "e"], ["p", "i", "g", "s"], ["g", "o", "a", "t", "s"], ["b", "a", "r", "n"], ["d", "o", "n", "k", "e", "y"], ["c", "o", "w", "s"], ["f", "i", "e", "l", "d"], ["t", "r", "u", "c", "k"], ["c", "r", "o", "p", "s"], ["g", "a", "r", "d", "e", "n"], ["p", "l", "o", "w"],
+  ];
 
-		// Clear Button
-      	//$("#clear").on("click", function() {
+      
+  var wordBlank = [
+             " _ ",
+             " _ ",
+             " _ ",
+             " _ ",
+             " _ ",
+             " _ ",
+           ];
+    
+
+    var random = Math.floor((Math.random()*(wordList.length))); 
+    var wordCurrent = wordList[random];
+    var blank = function(){
+    
+      if (wordCurrent.length===6){
+        $("#wordBlanks").html("<span id='blank1'>" + wordBlank[0] + "</span>" + "<span id='blank2'>" +wordBlank[1] + "</span>" + "<span id='blank3'>" +wordBlank[2] + "</span>" + "<span id='blank4'>" +wordBlank[3] + "</span>" + "<span id='blank5'>" +wordBlank[4] + "</span>" + "<span id='blank6'>" +wordBlank[5] + "</span>");
+      }
+      else if (wordCurrent.length===5){
+        $("#wordBlanks").html("<span id='blank1'>" + wordBlank[0] + "</span>" + "<span id='blank2'>" +wordBlank[1] + "</span>" + "<span id='blank3'>" +wordBlank[2] + "</span>" + "<span id='blank4'>" +wordBlank[3] + "</span>" + "<span id='blank5'>" +wordBlank[4] + "</span>");
         
-        //$("#display").empty();
-
-
-
-      $(document).keyup(function() {
+      }
+      else if (wordCurrent.length===4){
+        $("#wordBlanks").html("<span id='blank1'>" + wordBlank[0] + "</span>" + "<span id='blank2'>" +wordBlank[1] + "</span>" + "<span id='blank3'>" +wordBlank[2] + "</span>" + "<span id='blank4'>" +wordBlank[3] + "</span>");
+      }
+  }
         
-        // Determines which key was pressed.
-      var userGuess = event.key;
+    
+  $("#click-me").on("click", function() {
+        
+        alert("Thank you for playing the farm edition of hangman.  All words relate to things you may see on a farm.  To get started, click ok and pick a letter you think may be in the word.  Good luck and enjoy the Game!");
+            
+    console.log(wordCurrent);
 
-		if(userGuess==="h" || userGuess==="o" || userGuess==="r" || userGuess==="s" || userGuess==="e") {
-          if (userGuess==="h") {
-            //letter to appear in the correct place
-            alert("Great job!  That letter is in the word.");
-            $("#user-text1").html("<h1>h</h1>");
+    blank ();
+    
+    });
+
+    
+  var match = function(){
+
+    var userGuess = $(document).keyup(function(){
+      
+      var userGuess = event.key; 
+      userGuess=userGuess.toLowerCase();
+
+      if(correctGuess<wordCurrent.length-1){
+        var gameOn = true;
+        }
+            
+      for (var i = 0; i < wordCurrent.length; i++){
+        if(wordCurrent[i]===userGuess){
+        var placeholder = true;
+        }
+      } 
+
+
+      if(placeholder){
+
+          if (userGuess===wordCurrent[0]){
+            $("#blank1").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!")
             }
-          else if (userGuess==="o") {
-            //letter to appear in the correct place
-            alert("You go!");
-            $("#user-text2").html("<h1>o</h1>");
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh.");
+              wins++;
+              $("#wins").html(wins);
             }
-          else if (userGuess==="r") {
-            //letter to appear in the correct place
-            alert("Bravo!");
-            $("#user-text3").html("<h1>r</h1>");
+            
+          } 
+          else if (userGuess===wordCurrent[1]){
+            $("#blank2").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!")
             }
-          else if (userGuess==="s") {
-            //letter to appear in the correct place
-            alert("You are a genius!");
-            $("#user-text4").html("<h1>r</h1>");
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh.");
+              wins++;
+              $("#wins").html(wins);
+              
             }
-          else if (userGuess==="e") {
-            //letter to appear in the correct place
-            alert("You are on fire!");
-            $("#user-text5").html("<h1>e</h1>");
+            
+          }
+          else if (userGuess===wordCurrent[2]){
+            $("#blank3").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!")
             }
-        else {
-	        alert("Oops!  That letter is not in the word.  Guess Again."); 
-	        //reflects changing guess count 
-	        guessCount--;
-	        //shows the wrong guesses, but need to code so that it shows every wrong guess
-	       	document.getElementById("missed1").innerHTML = userGuess;
-	       	//reflects new guess count, but need to code so that does not double count wrong guesses
-	       	document.getElementById("numGuess").innerHTML = guessCount;
-           }
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh.");
+              wins++;
+              $("#wins").html(wins);
+              
+            }
+            
+          }
+          else if (userGuess===wordCurrent[3]){
+            $("#blank4").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!");
+            }
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh.");
+              wins++;
+              $("#wins").html(wins);
+      
+            }
+            
+          }
+          else if (userGuess===wordCurrent[4]){
+            $("#blank5").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!")
+            }
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh.");
+              wins++;
+              $("#wins").html(wins);
+              
+            }
+            
+          }
+          
+          else if (userGuess===wordCurrent[5]){
+            $("#blank6").html(userGuess);
+            correctGuess++;
+            if (gameOn){
+            console.log(correctGuess);
+            alert("Awesome!  You guessed a correct letter. Keep guessing!")
+            }
+            else {
+              $("#word").html("YOU WIN!!  Hit Refresh and 'click me' to generate a new word.");
+              wins++;
+              $("#wins").html(wins);
+              
+            }
+            
+          }
+      
+      }
+    
+        if (!placeholder){
+          if(guessCount > 0){
+            alert("Oops!  That letter is not in the word.  Guess Again."); 
+            guessCount--;
+            $("#numGuess").html(guessCount);
+            $("#wrong").append(userGuess + " ");
+            usedGuess.push(userGuess);
+            for (var i=0; i < usedGuess.length; i++) {
+              if(userGuess===usedGuess[i]){
+                var used=true;
+                console.log("true");
+              }
+            }
+            if (used){
+              $("#noPlace").append(userGuess + " ");
+              $("#noPlace").hide();
+            }
 
-}
-	
+          }
+            else{
+            alert("Sorry, you lose. Please hit your browser's refresh button and then click on 'Click Me'."); 
+            $("#noPlace").append(userGuess);
+            $("#noPlace").hide();
+          }
+        }
+    });
+  }
+    
+  match ();
+    
 
+  $("#clear").on("click", function() {
+                     
+    var guessCount=8;
+    $("#display2").empty();
+    $("#numGuess").html(guessCount);
+    $("#wins").html(wins);
+    //$("#currentWord").empty();
+    //usedWordsList.push(wordCurrent);
+    //newWord();
+    
+  }); 
 
+  var newWord = function(){
+    var generateNew = function(){
+      var random = Math.floor((Math.random()*(wordList.length)));
+      wordCurrent=wordList[random];
+    }
+    generateNew();
+    console.log(wordCurrent);
+    blank();    
+    //match();
+  }
 
-
-
-
+}); 
+    
 
 
 
