a93f5bd853ea3b5d6047ec97fab9de5ae7748590
Lisa Ewart <ewartle@gmail.com>
2017-11-26 18:45:43 -0500

    This updates all files to improve game.

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
       
-    
+
+
   var wins=0;
   var correctGuess=0;
   var guessCount=8;
-  var usedWordsList =[];
+  //var usedWordsList =[];
   var usedGuess = [];
   
-  var wordList = [ ["h", "o", "r", "s", "e"], ["p", "i", "g", "s"], ["g", "o", "a", "t", "s"], ["b", "a", "r", "n"], ["d", "o", "n", "k", "e", "y"], ["c", "o", "w", "s"], ["f", "i", "e", "l", "d"], ["t", "r", "u", "c", "k"], ["c", "r", "o", "p", "s"], ["g", "a", "r", "d", "e", "n"], ["p", "l", "o", "w"],
+  var wordList = [ ["f", "a", "r", "m", "e", "r"], ["h", "o", "r", "s", "e"], ["p", "i", "g", "s"], ["g", "o", "a", "t", "s"], ["b", "a", "r", "n"], ["d", "o", "n", "k", "e", "y"], ["c", "o", "w", "s"], ["f", "i", "e", "l", "d"], ["t", "r", "u", "c", "k"], ["c", "r", "o", "p", "s"], ["g", "a", "r", "d", "e", "n"], ["p", "l", "o", "w"],
   ];
 
       
@@ -22,11 +23,13 @@ $(document).ready(function() {
     
   var random = Math.floor((Math.random()*(wordList.length))); 
   var wordCurrent = wordList[random];
-  function pushWords(){
-    usedWordsList.push(wordCurrent);
-  };
-  pushWords();
+  //function pushWords(){
+  //  usedWordsList.push(wordCurrent);
+  //};
+  //pushWords();
 
+
+  
     
    var blank = function(){
     
@@ -41,16 +44,39 @@ $(document).ready(function() {
         $("#wordBlanks").html("<span id='blank1'>" + wordBlank[0] + "</span>" + "<span id='blank2'>" +wordBlank[1] + "</span>" + "<span id='blank3'>" +wordBlank[2] + "</span>" + "<span id='blank4'>" +wordBlank[3] + "</span>");
       }
     }
+
+    blank();
+
+
+
+$("#click-me").on("click", function() {
+    
+	    
+	  correctGuess=0;
+      guessCount=8;
+      $("#numGuess").html(guessCount);
+      usedGuess = [];	
+      $("#wrong").html(usedGuess);
+      $("#word").html("???????");
+      console.log(usedGuess);
+	  random = Math.floor((Math.random()*(wordList.length)));
+	  wordCurrent = wordList[random];
+	  console.log(wordCurrent);
+	  blank();
+	   //$("#display2").empty();
+
+	   
+	});   
         
     
-  $("#click-me").on("click", function() {
+//  $("#click-me").on("click", function() {
         
-        alert("Thank you for playing the farm edition of hangman.  All words relate to things you may see on a farm.  To get started, click ok and pick a letter you think may be in the word.  Good luck and enjoy the Game!");
-        blank ();
+//        alert("Thank you for playing the farm edition of hangman.  All words relate to things you may see on a farm.  To get started, click ok and pick a letter you think may be in the word.  Good luck and enjoy the Game!");
+     //   blank ();
         
         
         
-   });
+//   });
   
 
   //this is the function that matches userguess to letters in the word and determines what happens in html.
@@ -85,9 +111,11 @@ $(document).ready(function() {
         }
 
         if (yes&&!gameOn&&!alreadyGuess){
-          $("#word").html("YOU WIN!!  Hit Refresh.");
+          $("#word").html("YOU WIN!!  Click on 'New Word'.");
           wins++;
           $("#wins").html(wins);
+          
+         
         }
 
         if(yes){
@@ -97,7 +125,7 @@ $(document).ready(function() {
               usedGuess.push(userGuess);
               }
               pushGuess();
-              alert("Awesome:  You guessed a correct letter.");
+              
               
               if (userGuess===wordCurrent[0]){
                 $("#blank1").html(userGuess);
@@ -118,11 +146,10 @@ $(document).ready(function() {
                 $("#blank6").html(userGuess);
               }   
             }
-          }
+        }
           
         
         if (!yes&&!alreadyGuess){
-            alert("Oops!  That letter is not in the word.  Guess Again."); 
             guessCount--;
             var pushGuess = function(){
             usedGuess.push(userGuess);
@@ -132,11 +159,15 @@ $(document).ready(function() {
             $("#wrong").append(userGuess + " ");
             }
        
-        if (!count){
-            alert("Sorry, you lose. Please hit your browser's refresh button and then click on 'Click Me'."); 
+        if (!yes&&!count&&!alreadyGuess){
+            alert("Sorry, you lose. Please click on 'New Word'."); 
             $("#noPlace").append(userGuess);
             $("#noPlace").hide(); 
+            guessCount=0;
+            $("#numGuess").html(guessCount);
             }
+
+
         if (alreadyGuess){
                 alert("You have already selected that letter, pick again.")
               }      
@@ -176,7 +207,11 @@ $(document).ready(function() {
     //blank();    
     //match();
   //}
+
+ //}
   match();
+
+
 
 }); 
     
